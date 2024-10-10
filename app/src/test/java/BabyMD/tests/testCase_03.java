43347030efd157502950209551f779469cb90f0c
Shubhi <shubhig389@gmail.com>
2024-10-10 15:43:09 +0530

    coverage report with updated testcases

--- app/src/test/java/BabyMD/tests/testCase_03.java
+++ app/src/test/java/BabyMD/tests/testCase_03.java
@@ -1,64 +1,64 @@
-// package BabyMD.tests;
+package BabyMD.tests;
 
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.openqa.selenium.chrome.ChromeOptions;
-// import org.testng.annotations.AfterSuite;
-// import org.testng.annotations.BeforeSuite;
-// import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
-// import BabyMD.DP;
-// import BabyMD.pages.Login;
-// import BabyMD.pages.PatientDashBoard;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import BabyMD.DP;
+import BabyMD.pages.Login;
+import BabyMD.pages.PatientDashBoard;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-// public class testCase_03 {
-//     static ChromeDriver driver;
-//     String Doctorname="Doc Sanjay";
-//          @BeforeSuite(alwaysRun=true)
-//         public static void createDriver() {
-//             System.out.println("Initialising the driver");
-//             WebDriverManager.chromedriver().setup();
+public class testCase_03 {
+    static ChromeDriver driver;
+    String Doctorname="Doc Sanjay";
+         @BeforeSuite(alwaysRun=true)
+        public static void createDriver() {
+            System.out.println("Initialising the driver");
+            WebDriverManager.chromedriver().setup();
     
-//             // Configure driver to start as headless
-//             ChromeOptions options = new ChromeOptions();
-//            // options.addArguments("--headless", "--window-size=1920,1200");
+            // Configure driver to start as headless
+            ChromeOptions options = new ChromeOptions();
+           // options.addArguments("--headless", "--window-size=1920,1200");
     
-//              driver = new ChromeDriver(options);
-//          driver.manage().window().maximize();
+             driver = new ChromeDriver(options);
+         driver.manage().window().maximize();
       
-//     }
-//     @Test(description="Adding Prescription to Patient",dataProvider = "data-Provider",dataProviderClass = DP.class, priority = 3, groups = {"prescription"})
-//     public void TestCase03(String DoctorName,String Diagonis,String MedicineName,String CourseDuration,String Frequency,String Regimen,String Advice) throws InterruptedException {
-//         System.out.println("Test case 03 started");
-//                  Login login= new Login(driver);
-//                 login.navigateToLoginPage();
-//                 Thread.sleep(3000);
-//                 login.performLogin();
-//         PatientDashBoard dashBoard=new PatientDashBoard(driver);
-//         dashBoard.Patienturl();
-//         dashBoard.navigateToLandingPage();
-//         Thread.sleep(2000);
-//         dashBoard.AssignDoctor(Doctorname);
-//         Thread.sleep(2000);
-//         dashBoard.AssignDoctor("Doc Sanjay");
-//         Thread.sleep(2000);
-//         dashBoard.Prescription();
-//         Thread.sleep(2000);
-//         dashBoard.clickMedicine();
-//         Thread.sleep(2000);
-//         dashBoard.CourseTime();
-//         Thread.sleep(2000);
-//         dashBoard.Selectfrequency();
-//         Thread.sleep(2000);
-//         dashBoard.selectregimen();
-//         Thread.sleep(2000);
-//         dashBoard.Save();
-//     }
-//     @AfterSuite
-//     public static void quitDriver() {
-//         System.out.println("quit()");
+    }
+    @Test(description = "Adding Prescription",dataProvider = "data-Provider",dataProviderClass = DP.class, priority = 3, groups = {"Prescription"})
+    public void TestCase03(String DoctorName,String Diagonis,String MedicineName,String CourseDuration,String Frequency,String Regimen,String Advice) throws InterruptedException {
+        System.out.println("Test case 03 started");
+                 Login login= new Login(driver);
+                login.navigateToLoginPage();
+                Thread.sleep(3000);
+                login.performLogin();
+        PatientDashBoard dashBoard=new PatientDashBoard(driver);
+        dashBoard.Patienturl();
+        dashBoard.navigateToLandingPage();
+        Thread.sleep(2000);
+        dashBoard.AssignDoctor(Doctorname);
+        Thread.sleep(2000);
+        dashBoard.AssignDoctor("Doc Sanjay");
+        Thread.sleep(2000);
+        dashBoard.Prescription();
+        Thread.sleep(2000);
+        dashBoard.clickMedicine();
+        Thread.sleep(2000);
+        dashBoard.CourseTime();
+        Thread.sleep(2000);
+        dashBoard.Selectfrequency();
+        Thread.sleep(2000);
+        dashBoard.selectregimen();
+        Thread.sleep(2000);
+        dashBoard.Save();
+    }
+    @AfterSuite
+    public static void quitDriver() {
+        System.out.println("quit()");
 
-//         driver.quit();
+        driver.quit();
      
-//     }
-// }
+    }
+}
--- app/src/test/java/BabyMD/tests/testCase_04.java
+++ app/src/test/java/BabyMD/tests/testCase_04.java
@@ -1,72 +1,73 @@
-// package BabyMD.tests;
+package BabyMD.tests;
 
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.openqa.selenium.chrome.ChromeOptions;
-// import org.testng.annotations.AfterSuite;
-// import org.testng.annotations.BeforeSuite;
-// import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
-// import BabyMD.pages.Login;
-// import BabyMD.pages.Settings;
-// import io.github.bonigarcia.wdm.WebDriverManager;
+import BabyMD.DP;
+import BabyMD.pages.Login;
+import BabyMD.pages.Settings;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-// public class testCase_04 {
-//      static ChromeDriver driver;
+public class testCase_04 {
+     static ChromeDriver driver;
  
-//          @BeforeSuite(alwaysRun=true)
-//         public static void createDriver() {
-//             System.out.println("Initialising the driver");
-//             WebDriverManager.chromedriver().setup();
+         @BeforeSuite(alwaysRun=true)
+        public static void createDriver() {
+            System.out.println("Initialising the driver");
+            WebDriverManager.chromedriver().setup();
     
-//             // Configure driver to start as headless
-//             ChromeOptions options = new ChromeOptions();
-//            // options.addArguments("--headless", "--window-size=1920,1200");
+            // Configure driver to start as headless
+            ChromeOptions options = new ChromeOptions();
+           // options.addArguments("--headless", "--window-size=1920,1200");
     
-//              driver = new ChromeDriver(options);
-//          driver.manage().window().maximize();
+             driver = new ChromeDriver(options);
+         driver.manage().window().maximize();
       
-//     }
-//     @Test
-//     public void TestCase04() throws InterruptedException {
-//         System.out.println("Test case 04 started");
-//                  Login login= new Login(driver);
-//                 login.navigateToLoginPage();
-//                 Thread.sleep(3000);
-//          login.performLogin();
-//                 Thread.sleep(3000);
-//                 Settings goal=new Settings(driver);
-//         goal.navigateToLandingPage();
-//         Thread.sleep(3000);
-//         goal.addNewGoal();
-//         Thread.sleep(3000);
-//         goal.AddGoalDetails();
-//         Thread.sleep(3000);
-//         goal.frequencySelect();
-//         Thread.sleep(3000);
-//         goal.AddNotification();
-//         Thread.sleep(3000);
-//         goal.AddReminder();
-//         Thread.sleep(2000);
-//         goal.saveDetails();
+    }
+    @Test(description = "Adding Goal",dataProvider = "data-Provider",dataProviderClass = DP.class, priority = 4, groups = {"Goal Creation"})
+    public void TestCase04(String GoalName,String tagetValue,String Frequency,String Notification,String Reminder) throws InterruptedException {
+        System.out.println("Test case 04 started");
+                 Login login= new Login(driver);
+                login.navigateToLoginPage();
+                Thread.sleep(3000);
+         login.performLogin();
+                Thread.sleep(3000);
+                Settings goal=new Settings(driver);
+        goal.navigateToLandingPage();
+        Thread.sleep(3000);
+        goal.addNewGoal();
+        Thread.sleep(3000);
+        goal.AddGoalDetails();
+        Thread.sleep(3000);
+        goal.frequencySelect();
+        Thread.sleep(3000);
+        goal.AddNotification();
+        Thread.sleep(3000);
+        goal.AddReminder();
+        Thread.sleep(2000);
+        goal.saveDetails();
        
-//         // PatientDashBoard  patientDashBoard=new PatientDashBoard(driver);
-//         // patientDashBoard.navigateToLandingPage();
-//         // patientDashBoard.goalcreation();
-//         // Thread.sleep(3000);
-//         // goal.gotogoal();
-//         // Thread.sleep(3000);
-//         // goal.goalname();
-//         // Thread.sleep(3000);
-//         // goal.saveDetails();
+        // PatientDashBoard  patientDashBoard=new PatientDashBoard(driver);
+        // patientDashBoard.navigateToLandingPage();
+        // patientDashBoard.goalcreation();
+        // Thread.sleep(3000);
+        // goal.gotogoal();
+        // Thread.sleep(3000);
+        // goal.goalname();
+        // Thread.sleep(3000);
+        // goal.saveDetails();
    
-//     }
-//     @AfterSuite
-//     public static void quitDriver() {
-//         System.out.println("quit()");
+    }
+    @AfterSuite
+    public static void quitDriver() {
+        System.out.println("quit()");
 
-//         driver.quit();
+        driver.quit();
      
-//     }
-// }
+    }
+}
 
 
